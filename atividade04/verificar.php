58ce4df109a8b6b8b5e021ffc62504b418098f85
MatheusKan <matheuskan@hotmail.com>
2024-03-15 22:33:38 -0300

    Update

--- atividade04/verificar.php
+++ atividade04/verificar.php
@@ -2,7 +2,7 @@
     $A = $_POST["a"];
     $B = $_POST["b"];
     $C = $_POST["c"];
-    if ($A + $B >= $C and $A + $C >= $B and $B + $C >= $A) {
+    if ($A + $B >= $C and $A + $C >= $B and $B + $C >= $A and $A > 0 and $B > 0 and $C > 0) {
         if ($A == $B and $A == $C) {
             echo "equilátero";
         }elseif ($A != $B and $A != $C and $B != $C) {
